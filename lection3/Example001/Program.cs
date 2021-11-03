760fd18cb79bdfed93941d545a39ab2a5894aac1
Pablos <pablos@list.ru>
2021-11-03 14:13:56 +0300

    lection3 char replace

---
+++ lection3/Example001/Program.cs
@@ -0,0 +1,33 @@
+﻿// Таблица умножения
+
+// for (int i = 2; i < 10; i++)
+// {
+//     for (int f = 2; f <= 10; f++)
+//     {
+//         Console.WriteLine($"{i} X {f} = {f*(i)}");
+//     }
+//     Console.WriteLine(" ");
+// }
+
+// Дан текст. В тексте нужно заменить все пробелы на | , маленькие п заменить на П , 
+// большие К заменить на маленькие к.
+
+// string textt = "Дан текст. В тексте нужно заменить все пробелы на | , маленькие п заменить на П , большие К заменить на маленькие к.";
+
+// string replace(string textt, char one, char two)
+// {
+//     string resultt = String.Empty;
+//     for (int i = 0; i < textt.Length; i++)
+//     {
+//         if (textt[i] == one) resultt = resultt + two;
+//         else resultt = resultt + textt[i];
+//     }
+//     return resultt;
+// }
+// Console.WriteLine(replace(textt, ' ', '|'));
+// Console.WriteLine(replace(textt, 'п', 'П'));
+// Console.WriteLine(replace(textt, 'К', 'к'));
+
+// Сортировка выбором
+
+
