76a42898f41e733a1f3ad9d9111fb368a2c5b988
Pablos <pablos@list.ru>
2021-11-08 22:47:29 +0300

    solved example 17 from junior

--- lection3/Example004-junior/Program.cs
+++ lection3/Example004-junior/Program.cs
@@ -7,11 +7,52 @@
 // }
 // Krat(161, 7, 23);
 
-// 16. Дано число обозначающее день недели. Выяснить является номер недели выходным днём
+// bool Kratno(int a, int b, int c)
+// {
+//     return (a % b % c == 0);
+// }
+// if (Kratno(322, 7, 23)) Console.Write("Кратно");
+// else Console.Write("Не кратно");
 
+// 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным днём
 
+// bool Day(int n)
+// {
+//     return (n == 6 || n == 7);
+// }
+// if (Day(5)) Console.WriteLine("Выходной");
+// else Console.WriteLine("Будний день");
 
 // 17. По двум введённым числам проверять является ли одно квадратом другого
+
+// Вариант "Скучный"
+// bool IfQuad(int a, double b)
+// {
+//     return (Math.Sqrt(a) == b);
+// }
+//     if (IfQuad(100, 10)) Console.WriteLine ("Является квадратом");
+//     else Console.WriteLine ("Не является квадратом");
+
+// Вариант "интересный"
+
+// string OutLine(int[] num)
+// {
+//     if (Math.Sqrt(num[0]) == num[1]) return ($"Число {num[0]} является квадратом числа {num[1]}");
+//     else return ($"Число {num[0]} НЕ является квадратом числа {num[1]}");
+// }
+
+// int[] Enter()
+// {
+//     int[] num=new int[2];
+//     Console.WriteLine("Введите первое число ");
+//     num[0] = int.Parse(Console.ReadLine());
+//     Console.WriteLine("Введите второе число ");
+//     num[1] = int.Parse(Console.ReadLine());
+//     return (num);
+// }
+
+// Console.WriteLine(OutLine(Enter()));
+
 // 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
 // 19. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
 // 20. Ввести номер четверти, показать диапазоны для возможных координат
