2b83f9ebc442a9bcdaa1bd80a77ca02fb6960541
Pablos <pablos@list.ru>
2021-11-16 17:44:55 +0300

    43 solved

--- lection3/Example008-tlead/Program.cs
+++ lection3/Example008-tlead/Program.cs
@@ -1,6 +1,12 @@
 ﻿// 41. Выяснить являются ли три числа сторонами треугольника 
+// В треугольнике сумма длин двух любых сторон больше длины третьей стороны
 
+// bool TrianglRule(int a, int b, int c)
+// {
+//     return a + b > c && a + c > b && b + c > a;
+// }
 
+// Console.Write(TrianglRule(5, 8, 6));
 
 // 42. Определить сколько чисел больше 0 введено с клавиатуры
 
@@ -20,7 +26,57 @@
 
 // 43. Написать программу преобразования десятичного числа в двоичное
 
+//  сам через массив
+// int ArrLength(int arg) // length of new number
+// {
+//     int res = 0;
+//     while (arg > 0)
+//     {
+//         arg /= 2;
+//         res++;
+//     }
+//     return res;
+// }
 
+// int[] Converter(int arg)
+// {
+//     int[] res = new int[ArrLength(arg)];
+//     for (int i = res.Length - 1; i >= 0; i--)
+//     {
+//         res[i] = arg % 2;
+//         arg /= 2;
+//     }
+//     return res;
+// }
+
+// Console.Write(string.Join("", Converter(20)));
+
+// через строку
+
+// string Converter(int arg)
+// {
+//     string res = string.Empty;
+//     while (arg > 0)
+//     {
+//         res = (arg % 2) + res;
+//         arg /= 2;
+//     }
+//     return res;
+// }
+
+// Console.Write(Converter(20));
+
+// рекурсивно подсмотрел
+// string FuncTo2(int a)
+// {
+//     if (a == 1) return "1";
+//     else return FuncTo2(a / 2) + (a % 2);
+// }
+// Console.Write(FuncTo2(4));
+
+//  конвертер вшитый в C#
+// int x = 4;
+// Console.Write(Convert.ToString(x, 2)); // 2 - основание системы счисления
 
 // 44. Найти точку пересечения двух прямых заданных уравнением y=kx+b, а1 k1 и а2 и k2 заданы
 
