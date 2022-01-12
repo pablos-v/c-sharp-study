4ef5503a17656c78f296c72ce9e7465777263694
Pablos <pablos@list.ru>
2022-01-12 16:42:24 +0300

    CS50s Credit card validation ready

--- testingSomeCode/Program.cs
+++ testingSomeCode/Program.cs
@@ -1,4 +1,4 @@
-﻿// find min index in sorted array with binary search
+﻿// ------------------find min index in sorted array with binary search----------
 
 // int[] array = { 3, 4, 5, 6, 7, 8, 9, 0, 1, 2 };
 
@@ -18,7 +18,7 @@
 // Console.Write(FindMinIndex(array));
 
 
-// write Mario tower from CS50
+// ------------- write Mario tower from CS50
 
 // void Mario() //1st version
 // {
@@ -38,29 +38,73 @@
 
 // Mario();
 
-// 2nd version
+// --------------2nd version Mario-----------------
 
-string Loop(string a, int num)
+// string Loop(string a, int num)
+// {
+//     string h = string.Empty;
+//     for (int i = 0; i < num; i++) h += a;
+//     return h;
+// }
+
+// void Mario()
+// {
+//     int n = 0;
+//     while (n <= 0)
+//     {
+//         Console.WriteLine("Введите высоту блоков: ");
+//         if (int.TryParse(Console.ReadLine(), out int m)) n = m;
+//     }
+
+//     for (int f = 1; f < n + 1; f++)
+//     {
+//         string row = Loop(" ", n - f) + Loop("#", f) + Loop(" ", 2) + Loop("#", f) + Loop(" ", n - f);
+//         Console.Write(row + "\n");
+//     }
+// }
+
+// Mario();
+
+
+//-------------Credit card validation (by Luhn’s Algorithm)--------
+
+long[] EnterCardNum()
 {
-    string h = string.Empty;
-    for (int i = 0; i < num; i++) h += a;
-    return h;
+    while (true)
+    {
+        Console.WriteLine("Введите номер кредитной карты: ");
+        string a = Console.ReadLine();
+        int l = a.Length;
+        if (l >= 13 && l <= 16 && long.TryParse(a, out long m))
+        {
+            long[] arr = new long[l];
+            for (int i = l - 1; i >= 0; i--)
+            {
+                arr[i] = m % 10;
+                m /= 10;
+            }
+            return arr;
+        }
+        else Console.WriteLine("Номер введён не правильно!\n");
+    }
 }
 
-void Mario()
+bool IsValid(long[] array)
 {
-    int n = 0;
-    while (n <= 0)
+    long sum = 0;
+    for (int i = array.Length - 2; i >= 0; i -= 2)
     {
-        Console.WriteLine("Введите высоту блоков: ");
-        if (int.TryParse(Console.ReadLine(), out int m)) n = m;
+        long x = array[i] * 2;
+        if (x / 10 != 0) sum += (x % 10) + (x / 10);
+        else sum += x;
     }
-
-    for (int f = 1; f < n + 1; f++)
+    for (int i = array.Length - 1; i >= 0; i -= 2)
     {
-        string row = Loop(" ", n - f) + Loop("#", f) + Loop(" ", 2) + Loop("#", f) + Loop(" ", n - f);
-        Console.Write(row + "\n");
+        sum += array[i];
     }
+    Console.WriteLine("Контрольная цифра " + sum);
+    return sum % 10 == 0;
 }
 
-Mario();
+if (IsValid(EnterCardNum())) Console.Write("Номер кредитной карты подлинный!");
+else Console.Write("Кредитной карты с таким номером НЕ СУЩЕСТВУЕТ.");
