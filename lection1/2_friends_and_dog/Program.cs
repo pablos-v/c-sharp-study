23f157288a2a9cb776ad96b72d766407d67a7ccb
pablos-v <89704097+pablos-v@users.noreply.github.com>
2022-01-13 21:47:19 +0300

    Updated using methods

--- lection1/2_friends_and_dog/Program.cs
+++ lection1/2_friends_and_dog/Program.cs
@@ -1,41 +1,38 @@
-﻿Console.Write("Введите скорость первого друга: ");
-string firstFriendSpeed1 = Console.ReadLine();
-int firstFriendSpeed = int.Parse(firstFriendSpeed1);
-
-Console.Write("Введите скорость второго друга: ");
-string secondFriendSpeed1 = Console.ReadLine();
-int secondFriendSpeed = int.Parse(secondFriendSpeed1);
-
-Console.Write("Введите скорость собачки: ");
-string dogSpeed1 = Console.ReadLine();
-int dogSpeed = int.Parse(dogSpeed1);
-
-Console.Write("А какое расстояние между друзьями: ");
-string distance1 = Console.ReadLine();
-double distance = double.Parse(distance1);
-
-int count = 0;
-//double distance = 10000;
-//int firstFriendSpeed = 1;
-//int secondFriendSpeed = 2;
-//int dogSpeed = 5;
-int friend = 2;
-double time = 0;
-while (distance > 10)
+// Задача: 2 друга идут навстречу друг другу. Пока они идут, собака бегает от одного к другому.
+// Сколько раз собака пробежит от одного к другому на момент встречи друзей?
+int InputMethod(string s)
 {
-    if (friend == 1)
+    while (true)
     {
-        time = distance / (firstFriendSpeed + dogSpeed);
-        friend = 2;
+        Console.Write(s);
+        if (int.TryParse(Console.ReadLine(), out int n) && n > 0) return n;
+        else Console.WriteLine("Некорректное значение, давайте снова.");
     }
-    else
-    {
-        time = distance / (secondFriendSpeed + dogSpeed);
-        friend = 1;
-    }
-    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
-    count++;
 }
-Console.Write("Собака пробежит ");
-Console.Write(count);
-Console.Write(" раз.");
+int Counter()
+{
+    int firstFriendSpeed = InputMethod("Введите скорость первого друга: ");
+    int secondFriendSpeed = InputMethod("Введите скорость второго друга: ");
+    int dogSpeed = InputMethod("Введите скорость собаки: ");
+    double distance = InputMethod("А какое расстояние между друзьями: ");
+    int count = 0, friend = 2;
+    double time = 0;
+    while (distance > 10)
+    {
+        if (friend == 1)
+        {
+            time = distance / (firstFriendSpeed + dogSpeed);
+            friend = 2;
+        }
+        else
+        {
+            time = distance / (secondFriendSpeed + dogSpeed);
+            friend = 1;
+        }
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Собака пробежит " + Counter() + " раз.");
